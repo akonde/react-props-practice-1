32e7b35114a73a37ce21cc8660a25aaf014cd2a7
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-07-03 07:35:35 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { useState } from 'react'
-import { reverse } from './helpers'
+import { useState } from "react";
+import { reverse } from "./helpers";
 
 function App() {
-  const [name, setName] = useState('Nicolas')
+  const [name, setName] = useState("Nicolas");
 
-  return (
-    <div className="app">
-      <h1 className="title">Welcome, {name}!</h1>
+  function Title() {
+    return <h1 className="title">Welcome, {name}!</h1>;
+  }
+
+  function NameLength() {
+    return (
       <p className="name-length">
         Did you know that {name} is {name.length} characters long?!
       </p>
+    );
+  }
+
+  function NameReversed() {
+    return (
       <p className="name-reversed">
         Also, {name} backwards is {reverse(name)}
       </p>
+    );
+  }
+  return (
+    <div className="app">
+      <Title />
+      <NameLength />
+      <NameReversed />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
